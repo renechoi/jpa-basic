7e55c74e774606403edc9d1706dfbcf310f601e7
renechoi <renechoi90@gmail.com>
2023-03-18 11:56:19 +0900

    feat: jpa query example

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,32 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("helloA");
+            em.persist(member);
+        } catch (Exception e){
+            transaction.commit();;
+        }finally{
+            em.close();;
+        }
+
+
+        emf.close();
 
 
     }
